02a7fff202efe10bf662903dd4c32b6902e9f5f0
Vítor Assis <contatoassisvt@gmail.com>
2023-03-05 16:19:12 -0300

    Update Variaveis1.py

--- Variaveis1.py
+++ Variaveis1.py
@@ -1,3 +1,20 @@
+# Qual será o resultado dos respectivos prints?
 criaturas_maritmas = 'tubarão, foca, lula, camarão'
 print(criaturas_maritmas)
-# Qual será o resultado do print?
+--
+course = 'jennifer'
+print(course[1:-1])
+--
+course = 'Python for Beginners'
+print(course[:5])
+--
+course = 'Python for Beginners'
+print(course[0:])
+--
+course = 'Python for Beginners'
+another = course[:]
+print(another[:])
+--
+#Questão CBB
+lista = ["uva",["mamão", ("pêra","abacate")]]
+print(lista[-1][:1][0])
