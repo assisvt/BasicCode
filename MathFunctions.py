657f69bb1754a70ce3728013efe5a1e381a77e9d
Vítor Assis <contatoassisvt@gmail.com>
2023-04-09 17:57:46 -0300

    Create MathFunctions.py

---
+++ MathFunctions.py
@@ -0,0 +1,10 @@
+######## Função de arredondar o número ############
+x = 2.9
+print(round(x)) # Printa o número 3
+--
+
+########  Função que retorna sempre o número positivo ##########
+x = 2.9
+print(abs(-2.9)) # Retorna 2.9
+
+# ver outros em https://docs.python.org/pt-br/3/library/math.html
