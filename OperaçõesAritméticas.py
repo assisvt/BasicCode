409ed3a999c2320f2b2d7bd01e48b228ba42b81b
Vítor Assis <contatoassisvt@gmail.com>
2023-04-09 19:32:31 -0300

    Update OperaçõesAritméticas.py

--- OperaçõesAritméticas.py
+++ OperaçõesAritméticas.py
@@ -20,6 +20,15 @@ print(10 % 3) # Ao usar % (porcentagem), ao rodar o programa, será mostrado o r
 print(10 ** 3) # 1000, 10 ao cubo
 --
 
-raio = 2
-r3 = pow(raio, 3) # Raio elevado ao cubo. r3 é apenas uma variável
-print(r3)
+print('Digite o valor do raio para calcular o volume da esfera')
+import math # Importa o módulo de matemática para usarmos "math.pi = valor de pi"
+raio = float(input('Digite o valor do raio: '))
+r3 = pow(raio, 3) # Raio elevado ao cubo
+volume = 4 * math.pi * r3/3
+print(f'O volume da esfera, de raio {raio}, é', volume)
+--
+print('Digite o valor do raio para calcular o volume da esfera')
+import math # Importa o módulo de matemática para usarmos "math.pi = valor de pi"
+raio = float(input('Digite o valor do raio: '))
+volume = 4 * math.pi * raio**3/3
+print(f'O volume da esfera, de raio {raio}, é', volume)
