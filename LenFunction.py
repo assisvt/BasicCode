5b7fcf802600af13d81f234076e7650ef18afc5f
Vítor Assis <contatoassisvt@gmail.com>
2023-04-09 17:28:50 -0300

    Create LenFunction.py

---
+++ LenFunction.py
@@ -0,0 +1,4 @@
+#To calculate the number of characters in this string, you can use a built in function called len. OBS: len is a general purpose function, so it's not limited to counting the numbers of characters in a string. EX.: we can also use len to count the numbers of items in a list. So, it's a general purpose.
+course = 'Python for Begginers'
+print(len(course))
+#len and print are general purpose functions, they don't belong to strings or numbers or other kinds of objects like methods do
