f31a32b9c49dd4a2906f988f54f7fa8e098d3a22
Vítor Assis <contatoassisvt@gmail.com>
2023-04-09 18:38:35 -0300

    Create OperadoresLógicos.py

---
+++ OperadoresLógicos.py
@@ -0,0 +1,14 @@
+########### Operador and #################
+has_high_income = True
+has_good_credit = True
+
+if has_high_income and has_good_credit:
+    print("Elegible for loan")
+--
+
+########### Operador or ##################
+has_high_income = True
+has_good_credit = False
+
+if has_high_income or has_good_credit:
+    print("Elegible for loan")
