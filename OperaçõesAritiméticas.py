924a352b6a7e47f6df042b2388b5686591a5f5e6
Vítor Assis <contatoassisvt@gmail.com>
2023-04-09 17:52:50 -0300

    Create OperaçõesAritiméticas.py

---
+++ OperaçõesAritiméticas.py
@@ -0,0 +1,25 @@
+########### SOMA ###################
+print(5 + 3)
+--
+
+############# SUBTRAÇÃO ###############
+print(5 - 3)
+--
+
+############## Divisão ##############
+print(10 / 3) # aparecerá o float 3.3333333333333335
+--
+
+print(10 // 3) ##Se usar duas barrar obtém-se a aproximação.
+-- 
+
+print(10 % 3) # Ao usar % (porcentagem), ao rodar o programa, será mostrado o resto da divisão
+--
+
+################ EXPOENTE ################
+print(10 ** 3) # 1000, 10 ao cubo
+--
+
+raio = 2
+r3 = pow(raio, 3) # Raio elevado ao cubo. r3 é apenas uma variável
+print(r3)
